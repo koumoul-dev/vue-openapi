243353447fe6fa35a754785177375d760dfea844
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2016-12-01 00:58:27 +0100

    initial commit

---
+++ OpenAPI.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  Component here
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
---
+++ README.md
@@ -0,0 +1,1 @@
+An openAPI viewer component for vue.js v2, built with vue-material
