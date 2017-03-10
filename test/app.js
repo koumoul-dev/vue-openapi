a99becf9aed2f5d3600e82b82d708f05ea87ecf7
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-03-10 11:17:08 +0100

    test: example file is now OpenApi 3.0 valid, fixed paths

--- test/app.js
+++ test/app.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
-import OpenApi from './OpenApi.vue'
+import OpenApi from '../OpenApi.vue'
 import 'vue-material/dist/vue-material.css'
+import VueResource from 'vue-resource'
 
-import jsonApi from './swagger.json'
+import jsonApi from './petstore.json'
 
+Vue.use(VueResource)
 Vue.use(VueMaterial)
 Vue.material.registerTheme('default', {
   primary: 'black',
   accent: 'red'
 })
 
+console.log(Vue.version)
 new Vue({
   el: '#app',
   template: '<open-api :api="jsonApi" md-theme="\'default\'"></open-api>',
