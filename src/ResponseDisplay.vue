bd02b5ab5e7b602e1d74653ed07618c9e47f2793
Alban Mouton <alban.mouton@gmail.com>
2018-03-26 21:03:15 +0200

    chore: denser and collapsible headers display

--- src/ResponseDisplay.vue
+++ src/ResponseDisplay.vue
@@ -2,9 +2,17 @@
   <md-layout md-column class="response-display">
     <span>{{entry.method.toUpperCase()}} {{response.url}}&nbsp;&nbsp;&nbsp;&nbsp;<md-chip :class="response.ok ? 'md-primary':'md-warn'">{{response.status}} {{response.statusText}}</md-chip></span>
     <br>
-    <md-subheader>Headers</md-subheader>
-    <md-list>
-      <md-list-item v-for="header in Object.keys(response.headers.map)">{{header}}: {{response.headers.map[header]}}</md-list-item>
+
+    <md-list class="md-dense">
+      <md-list-item>
+        <md-subheader style="padding-left:0px;">Headers</md-subheader>
+        <md-list-expand>
+        <md-list>
+        <md-list-item v-for="header in Object.keys(response.headers.map)">{{header}}: {{Array.isArray(response.headers.map[header]) ? response.headers.map[header].join(',') : response.headers.map[header]}}</md-list-item>
+</md-list>
+        </md-list-expand>
+
+      </md-list-item>
     </md-list>
     <md-subheader>Body</md-subheader>
     <pre>{{formattedBody}}</pre>
